05eefd3da78be6bfb4d5c7c6155c03b14b3dac59
Janusz Majnert <jmajnert@gmail.com>
2015-01-08 14:41:55 +0100

    Replaced the tainting mechanism
    The new node tainting first finds the topmost DOM element under the mouse
    pointer (by using mousemove events) and finds the leaf-most element that has an
    addressable id. Benefits: performance for large pages should improve
    dramatically. Drawbacks: not sure if this is the correct approach for floating
    elements that may be rendered outsire their DOM parents area.
    
    Not tested.

--- data/fragments.js
+++ data/fragments.js
@@ -1,9 +1,6 @@
 (function (){
-var id_list=[];
-var addressable_nodes=[];
 var color;
 var last_element=null;
-var doc;
 function clean_last_element(){
     if(last_element){
         last_element.style.backgroundColor=color;
@@ -12,9 +9,6 @@ function clean_last_element(){
 }
 function cleanup(){
     clean_last_element();
-    for(var i=0;i<addressable_nodes.length;i++){
-        addressable_nodes[i].removeEventListener("mouseover",taint,true);
-    }
 }
 function copy_url(e){
     var url=""+doc.location;
@@ -30,54 +24,25 @@ function copy_url(e){
     e.preventDefault();
     e.stopPropagation();
 }
-function taint(e){
+function taint(target){
     clean_last_element();
-    last_element=e.currentTarget;
-    color=e.currentTarget.style.backgroundColor;
-    e.currentTarget.style.backgroundColor="red";
-    e.currentTarget.addEventListener("click",copy_url,true);
+    last_element=target;
+    color=target.style.backgroundColor;
+    target.style.backgroundColor="red";
+    target.addEventListener("click",copy_url,true);
 }
-function append_unique(id){
-    if(id=="")return 1;
-    if(id_list.indexOf(id)==-1){
-        id_list.push(id);
-        return 0;
-    }
-    return 1;
-}
-function traverse(node){
-    var rv=append_unique(node.id);
-    if(rv==0){
-        addressable_nodes.push(node);
-    }
-    for(var i=0;i<node.children.length;i++){
-        traverse(node.children[i]);
-    }
-}
-function attach_event_handlers(){
-    for(var i=0;i<addressable_nodes.length;++i){
-        addressable_nodes[i].addEventListener("mouseover",taint,true);
-    }
-}
-function traverse_children(node){
-    for(var i=0;i<node.children.length;i++){
-        traverse(node.children[i]);
-    }
-}
-function process_anchors(anchors){
-    for(var i=0;i<anchors.length;i++){
-        var rv=append_unique(anchors[i].name);
-        if(rv==0){
-            addressable_nodes.push(anchors[i]);
-        }
-    }
+function handler(e){
+  var o=e.target;
+  while(o.id == "" || document.getElementById(o.id) != o){
+    //alert(o);
+    if(o.parentElement == document.body) return;
+    o=o.parentElement;
+  }
+  id=o.id;
+  taint(o);
 }
 function start(){
-    doc=document;
-    if(!doc)return;
-    traverse_children(doc.body);
-    process_anchors(doc.anchors);
-    attach_event_handlers();
+    document.body.addEventListener("mousemove",handler);
 }
 start();
 self.port.on("detach", function() {
