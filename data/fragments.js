10102b0fc34e86a4664717ca21ad00b248864ce3
Janusz Majnert <jmajnert@gmail.com>
2015-01-08 23:43:27 +0100

    Added anchors' support again
    Added support for anchors, fixed some errors, tested the new tainting.
    Also improved coding style a bit, so that JS people don't bleed their eyes out.

--- data/fragments.js
+++ data/fragments.js
@@ -1,48 +1,61 @@
 (function (){
 var color;
-var last_element=null;
-function clean_last_element(){
-    if(last_element){
-        last_element.style.backgroundColor=color;
-        last_element.removeEventListener("click",copy_url,true);
+var lastElement = null;
+var identifier;
+
+function cleanLastElement(){
+    if(lastElement){
+        lastElement.style.backgroundColor = color;
+        lastElement.removeEventListener("click", copyUrl, true);
     }
 }
 function cleanup(){
-    clean_last_element();
+    cleanLastElement();
+    stop();
 }
-function copy_url(e){
-    var url=""+doc.location;
-    var idx=url.indexOf("#");
-    if(idx>0){
-        url=url.slice(0,idx);
+function copyUrl(e){
+    var url = "" + document.location;
+    var idx = url.indexOf("#");
+    if(idx > 0){
+        url = url.slice(0, idx);
     }
-    var identifier=last_element.id;
-    if(id_list.indexOf(identifier)==-1)identifier=last_element.name;
-    url+="#"+identifier;
-    self.port.emit("URL",url);
+    url += "#" + identifier;
+    self.port.emit("URL", url);
     cleanup();
     e.preventDefault();
     e.stopPropagation();
 }
 function taint(target){
-    clean_last_element();
-    last_element=target;
-    color=target.style.backgroundColor;
-    target.style.backgroundColor="red";
-    target.addEventListener("click",copy_url,true);
+    cleanLastElement();
+    lastElement = target;
+    color = target.style.backgroundColor;
+    target.style.backgroundColor = "red";
+    target.addEventListener("click", copyUrl, true);
+}
+function howToReachElement(o){
+    if(o.id != "" && document.getElementById(o.id) == o) return o.id;
+    if(o.tagName == "a" && o.name != "" && document.getElementById(o.name) == null){
+        for(var i = 0; i < document.anchors.length; i++){
+            if(document.anchors[i].name == o.name) return o.name;
+        }
+    }
+    return null;
 }
 function handler(e){
-  var o=e.target;
-  while(o.id == "" || document.getElementById(o.id) != o){
-    //alert(o);
-    if(o.parentElement == document.body) return;
-    o=o.parentElement;
-  }
-  id=o.id;
-  taint(o);
+    var o = e.target;
+    identifier = howToReachElement(o);
+    while(identifier == null){
+        o = o.parentElement;
+        if(o == document.body) return;
+        identifier = howToReachElement(o);
+    }
+    taint(o);
 }
 function start(){
     document.body.addEventListener("mousemove",handler);
+}
+function stop(){
+    document.body.removeEventListener("mousemove",handler);
 }
 start();
 self.port.on("detach", function() {
